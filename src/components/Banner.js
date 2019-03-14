0213d8b83bf02e23640ed623de0cc055fc3c3ea9
victoria517 <victoria.517.ramirez@gmail.com>
2019-03-14 18:03:50 -0500

    completed app

---
+++ src/components/Banner.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const style = {
+  logoIcon: {
+    width: 150,
+    heigth: 100
+  },
+  bgDark: {
+    backgroundColor: `rgb(1,0,1)`
+  }
+};
+
+const Banner = props => (
+  <article
+    style={style.bgDark}
+    className="mw7 center ph3 ph5-ns tc br2 pv3 bg-black-90 mb5 animated fadeIn"
+  >
+    <h2 className="fw2 f4 lh-copy mt0 mb3 white animated fadeIn slower">
+      Click on an image to earn points, but don't click on any twice!
+    </h2>
+  </article>
+);
+export default Banner;
