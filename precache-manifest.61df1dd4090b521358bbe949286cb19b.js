b9ab12196529d90c45e5a66cc4391d2782949968
victoria517 <victoria.517.ramirez@gmail.com>
2019-03-28 22:20:47 -0500

    Updates

---
+++ precache-manifest.61df1dd4090b521358bbe949286cb19b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab56db9d7e4ffb5f7581",
+    "url": "/Rainbow-Click-Game/static/js/runtime~main.2cdc999f.js"
+  },
+  {
+    "revision": "1c7e166ca429eebf797f",
+    "url": "/Rainbow-Click-Game/static/js/main.97dce7f1.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Rainbow-Click-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1c7e166ca429eebf797f",
+    "url": "/Rainbow-Click-Game/static/css/main.f669ce63.chunk.css"
+  },
+  {
+    "revision": "0466269f8ee1ce1b753300967e054e6d",
+    "url": "/Rainbow-Click-Game/index.html"
+  }
+];
